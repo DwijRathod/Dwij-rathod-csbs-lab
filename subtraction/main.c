ee54af26ab2255deecaf0252a1feb68b51c7a69c
Dwij Rathod <dwijrathod07@gmail.com>
2025-08-13 20:44:23 +0530

    Add files via upload

---
+++ subtraction/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+    
+    int num1,num2,diff;
+    
+    printf("enter first number : ");
+    scanf("%d",&num1);
+    
+    printf("enter second number : ");
+    scanf("%d",&num2);
+    
+    diff=num1-num2;
+    
+    printf("difference of two numbers is :  %d",diff);
+    
+    return 0;
+}
+

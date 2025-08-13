313966d60f20500369cb926e4113c34446eae8bd
Dwij Rathod <dwijrathod07@gmail.com>
2025-08-13 20:51:01 +0530

    Add files via upload

---
+++ multiplication/main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+    
+    int num1,num2,prod;
+    
+    printf("enter first number : ");
+    scanf("%d",&num1);
+    
+    printf("enter second number : ");
+    scanf("%d",&num2);
+    
+    prod=num1*num2;
+    
+    printf("product of two numbers is :  %d",prod);
+    
+    return 0;
+}
+
+
+

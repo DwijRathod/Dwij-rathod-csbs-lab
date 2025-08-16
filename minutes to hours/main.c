6ba4269f6676e0803dd210d7acc6e77a172a14d7
Dwij Rathod <dwijrathod07@gmail.com>
2025-08-16 14:07:16 +0530

    Add files via upload

---
+++ minutes to hours/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    
+    int hour,min;
+    
+    printf("enter minutes : ");
+    scanf("%d", &min);
+    
+    hour=min/60;
+    
+    printf("hours : %d",hour);
+    
+    return 0;
+}
+
+
+
